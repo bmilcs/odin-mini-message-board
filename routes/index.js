35ee6ee1e302d1843ed5889610463a58aa9a3dc4
Bryan Miller <bmilcs@yahoo.com>
2023-06-22 12:24:20 -0400

    Fix title for index route

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ const messages = [
 
 /* GET home page. */
 router.get("/", function (req, res) {
-  res.render("index", { title: "Express", messages: messages });
+  res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
 module.exports = router;
