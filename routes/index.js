91653667e14cc4f7ea75b563a4ef4747d4a59d53
Bryan Miller <bmilcs@yahoo.com>
2023-06-23 16:30:36 -0400

    Refactor index to use Message model

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require("express");
-var router = express.Router();
-var messages = require("../data/messages");
+const express = require("express");
+const router = express.Router();
+const Message = require("../models/message");
 
 /* GET home page. */
-router.get("/", function (req, res) {
+router.get("/", async function (req, res) {
+  const messages = await Message.find().sort({ added: 1 }).limit(10);
   res.render("index", { title: "Mini Message Board", messages: messages });
 });
 
