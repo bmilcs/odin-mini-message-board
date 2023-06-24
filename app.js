b8fd4693e62b0bd566bb5d5bd00422950dfce70d
Bryan Miller <bmilcs@yahoo.com>
2023-06-24 13:43:04 -0400

    Delete users route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
 const newRouter = require("./routes/new");
 
 var app = express();
@@ -32,7 +31,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // routes
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/new", newRouter);
 
 // catch 404 and forward to error handler
